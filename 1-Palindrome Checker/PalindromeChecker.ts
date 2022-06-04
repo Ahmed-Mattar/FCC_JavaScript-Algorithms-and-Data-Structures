ab3cb5f9fbb010e673ed6cd672ea9d1aab0119f8
Ahmed Mattar <AceMattar@gmail.com>
2022-06-04 19:53:27 +0200

    refactor: use static methods

--- 1-Palindrome Checker/PalindromeChecker.ts
+++ 1-Palindrome Checker/PalindromeChecker.ts
@@ -1,27 +1,21 @@
 class PalindromChecker {
-  string: string;
-
-  constructor(string: string) {
-    this.string = string;
+  private static removeNonAlphaNumeric(string: string) {
+    return string.replace(/[^a-z0-9]+/gi, "");
   }
 
-  private removeNonAlphaNumeric() {
-    return (this.string = this.string.replace(/[^a-z0-9]+/gi, ""));
+  private static toAllLower(string: string) {
+    return string.toLowerCase();
   }
 
-  private toAllLower() {
-    this.string = this.string.toLowerCase();
-  }
-
-  isPalindrom() {
-    this.removeNonAlphaNumeric();
-    this.toAllLower();
+  static isPalindrom(string: string) {
+    string = this.removeNonAlphaNumeric(string);
+    string = this.toAllLower(string);
 
     let pointer1 = 0;
-    let pointer2 = this.string.length - 1;
+    let pointer2 = string.length - 1;
 
     while (pointer1 < pointer2) {
-      if (this.string[pointer1] === this.string[pointer2]) {
+      if (string[pointer1] === string[pointer2]) {
         pointer1++;
         pointer2--;
         continue;
@@ -34,5 +28,5 @@ class PalindromChecker {
   }
 }
 
-let result = new PalindromChecker("not a palindrome").isPalindrom();
+let result = PalindromChecker.isPalindrom("0_0 (: /- :) 0-0");
 console.log(result);
